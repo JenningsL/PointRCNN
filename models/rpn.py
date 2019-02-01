ae1235c80ce36cf35cbd9acd134300a8f3ee27dc
liujianlin <liujianlin@imsightmed.com>
2019-02-01 10:49:20 +0800

    test seg with image

--- models/rpn.py
+++ models/rpn.py
@@ -129,16 +129,16 @@ class RPN(object):
                 self._img_preprocessed,
                 self._img_pixel_size,
                 self.is_training)
-
-        self.img_bottleneck = slim.conv2d(
-            self.img_feature_maps,
-            #128, [1, 1],
-            1, [1, 1],
-            scope='bottleneck',
-            normalizer_fn=slim.batch_norm,
-            normalizer_params={
-                'is_training': self.is_training})
-        return self.img_bottleneck
+        return self.img_feature_maps
+        # self.img_bottleneck = slim.conv2d(
+        #     self.img_feature_maps,
+        #     #128, [1, 1],
+        #     1, [1, 1],
+        #     scope='bottleneck',
+        #     normalizer_fn=slim.batch_norm,
+        #     normalizer_params={
+        #         'is_training': self.is_training})
+        # return self.img_bottleneck
 
     def get_segmentation_net(self, point_cloud, is_training, bn_decay, end_points):
         ''' 3D instance segmentation PointNet v2 network.
@@ -173,18 +173,8 @@ class RPN(object):
             64, [2.0,4.0], [16,32],
             [[256, 256, 512], [256,384, 512]],
             is_training, bn_decay, scope='layer4', bn=True)
-        '''
-        l5_xyz, l5_points, _ = pointnet_sa_module(l4_xyz, l4_points,
-            npoint=None, radius=None, nsample=None, mlp=[128,256,1024],
-            mlp2=None, group_all=True, is_training=is_training,
-            bn_decay=bn_decay, scope='layer5')
-        '''
 
         # Feature Propagation layers
-        '''
-        l4_points = pointnet_fp_module(l4_xyz, l5_xyz, l4_points, l5_points,
-            [128,128], is_training, bn_decay, scope='fa_layer1')
-        '''
         l3_points = pointnet_fp_module(l3_xyz, l4_xyz, l3_points, l4_points,
             [512,512], is_training, bn_decay, scope='fa_layer2', bn=True)
         l2_points = pointnet_fp_module(l2_xyz, l3_xyz, l2_points, l3_points,
@@ -197,7 +187,9 @@ class RPN(object):
         end_points['point_feats'] = tf.concat([l0_xyz,l0_points], axis=-1) # (B, N, 3+C1)
         end_points['point_feats_fuse'] = tf.concat([end_points['point_feats'], end_points['point_img_feats']], axis=-1) # (B, N, 3+C1+C2)
         # FC layers
-        net = tf_util.conv1d(l0_points, 128, 1, padding='VALID', bn=True,
+        # net = tf_util.conv1d(l0_points, 128, 1, padding='VALID', bn=True,
+        #     is_training=is_training, scope='conv1d-fc1', bn_decay=bn_decay)
+        net = tf_util.conv1d(end_points['point_img_feats'], 128, 1, padding='VALID', bn=True,
             is_training=is_training, scope='conv1d-fc1', bn_decay=bn_decay)
         net = tf_util.dropout(net, keep_prob=0.7,
             is_training=is_training, scope='dp1')
@@ -217,8 +209,10 @@ class RPN(object):
         bin_z_scores = tf.reduce_max(tf.nn.softmax(end_points['center_z_scores']), axis=-1) # (B,M)
         heading_scores = tf.reduce_max(tf.nn.softmax(end_points['heading_scores']), axis=-1) # (B,M)
         size_scores = tf.reduce_max(tf.nn.softmax(end_points['size_scores']), axis=-1) # (B,M)
-        confidence = seg_scores + bin_x_scores + bin_z_scores + heading_scores + size_scores
+        # confidence = seg_scores + bin_x_scores + bin_z_scores + heading_scores + size_scores
+        confidence = seg_scores * bin_x_scores * bin_z_scores * heading_scores * size_scores
         confidence.set_shape([batch_size, NUM_FG_POINT])
+        end_points['proposal_scores'] = confidence
         # BEV boxes
         boxes_3d = end_points['proposal_boxes'] # (B,M,8,3)
         corners_min = tf.gather(tf.reduce_min(boxes_3d, axis=2), [0,2], axis=-1)
@@ -275,7 +269,7 @@ class RPN(object):
         end_points = self.end_points
 
         with tf.device('/gpu:1'):
-            img_bottleneck = self.build_img_extractor() # (B,360,1200,C)
+            img_feature_maps = self.build_img_extractor() # (B,360,1200,C)
             pts2d = projection.tf_rect_to_image(tf.slice(point_cloud,[0,0,0],[-1,-1,3]), self.placeholders['calib'])
             pts2d = tf.cast(pts2d, tf.int32) #(B,N,2)
             indices = tf.concat([
@@ -283,7 +277,7 @@ class RPN(object):
                 tf.reshape(pts2d, [self.batch_size*self.num_point, 2])
             ], axis=-1) # (B*N,3)
             end_points['point_img_feats'] = tf.reshape(
-                tf.gather_nd(img_bottleneck, pts2d), # (B*N,C)
+                tf.gather_nd(img_feature_maps, pts2d), # (B*N,C)
                 [self.batch_size, self.num_point, -1])  # (B,N,C)
 
         with tf.device('/gpu:0'):
@@ -310,6 +304,7 @@ class RPN(object):
         # 3D Segmentation loss
         mask_loss = focal_loss(end_points['foreground_logits'], tf.one_hot(pls['seg_labels'], 2, axis=-1))
         tf.summary.scalar('mask loss', mask_loss)
+        return mask_loss, {}
         # gather box estimation labels of foreground points
         labels_fg = {}
         for k in pls.keys():
