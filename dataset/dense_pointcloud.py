c9ab5be50be36e0af684983b1cdd4df3dbcd684b
liujianlin <liujianlin@imsightmed.com>
2019-03-19 22:32:01 +0800

    generate senmantic mask from 3d label

--- dataset/dense_pointcloud.py
+++ dataset/dense_pointcloud.py
@@ -17,6 +17,8 @@ sys.path.append(os.path.join(ROOT_DIR, 'kitti'))
 sys.path.append(os.path.join(ROOT_DIR, 'utils'))
 sys.path.append(os.path.join(ROOT_DIR, 'visualize/mayavi'))
 from kitti_object import *
+from data_conf import g_type2onehotclass, type_whitelist, difficulties_whitelist
+from data_util import extract_pc_in_box3d
 # from viz_util import draw_lidar
 
 
@@ -87,8 +89,58 @@ class DensePoints(object):
             # draw_lidar(raw_pc, fig=fig, pts_color=(1, 1, 1))
             # raw_input()
 
+    def generate_semantic_mask(self, split):
+        output_dir = os.path.join(self.kitti_path, 'training/semantic_mask')
+        with open(os.path.join(self.kitti_path, split + '.txt')) as f:
+            frame_ids = [line.rstrip('\n') for line in f]
+        depth_map_dir = os.path.join(self.kitti_path, 'training/depth_map_dense')
+        if not os.path.isdir(output_dir):
+            os.mkdir(output_dir)
+        for fid in frame_ids:
+            fid = int(fid)
+            depth_map = np.load(os.path.join(depth_map_dir, '{:06}.npy'.format(fid)))
+            pc_dense = self.to_point_cloud(depth_map, fid)
+            objects = self.kitti_dataset.get_label_objects(fid)
+            image = self.kitti_dataset.get_image(fid)
+            calib = self.kitti_dataset.get_calibration(fid) # 3 by 4 matrix
+            objects = filter(lambda obj: obj.type in type_whitelist and obj.difficulty in difficulties_whitelist, objects)
+            pts_mask = {
+                'NonObject': np.zeros((len(pc_dense),), dtype=np.bool),
+                'Car': np.zeros((len(pc_dense),), dtype=np.bool),
+                'Pedestrian': np.zeros((len(pc_dense),), dtype=np.bool),
+                'Cyclist': np.zeros((len(pc_dense),), dtype=np.bool)
+            }
+            for obj in objects:
+                _,obj_box_3d = utils.compute_box_3d(obj, calib.P)
+                _,mask = extract_pc_in_box3d(pc_dense, obj_box_3d)
+                pts_mask[obj.type] = np.logical_or(mask, pts_mask[obj.type])
+            pts_mask['NonObject'] = (pts_mask['Car'] + pts_mask['Pedestrian'] + pts_mask['Cyclist']) == 0
+
+            semantic_mask = np.ones(image.shape[0:2], dtype=np.int32) * 255
+            # disp_mask = np.ones(image.shape, dtype=np.int32) * 255
+            # color_map = {
+            #     'NonObject': (0,0,0),
+            #     'Car': (0,0,142),
+            #     'Pedestrian': (220, 20, 60),
+            #     'Cyclist': (20,  255,  60)
+            # }
+            for k,v in pts_mask.items():
+                pts_2d = calib.project_rect_to_image(pc_dense[v, :3])
+                for u, v in pts_2d.astype(np.int32):
+                    if u >= image.shape[1] or v >= image.shape[0]:
+                        continue
+                    semantic_mask[v, u] = g_type2onehotclass[k]
+                    # disp_mask[v, u] = color_map[k]
+            # cv2.imshow('semantic_mask', disp_mask/255.0)
+            # cv2.waitKey(0)
+            fout = os.path.join(output_dir, '{:06}.png'.format(fid))
+            # np.save(fout, semantic_mask)
+            cv2.imwrite(fout, semantic_mask)
+            print('save', fid)
+
 if __name__ == '__main__':
     kitti_path = sys.argv[1]
     split = sys.argv[2]
     st = DensePoints(kitti_path)
-    st.generate_dense_points(split)
+    # st.generate_dense_points(split)
+    st.generate_semantic_mask(split)
